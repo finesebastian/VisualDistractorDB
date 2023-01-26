c6cf188c791e4e62623a6b44b2cf8e4eabec2028
cjm-64 <christopher.morris7164@gmail.com>
2023-01-26 16:26:54 -0500

    Added comments for what need sto be done

--- LocalImageMove.py
+++ LocalImageMove.py
@@ -1,6 +1,10 @@
 import shutil
 import sys
 
+# This script copies files to current directory
+# Need to add:
+#   Removal of old files
+
 if __name__ == "__main__":
     for num in range(16):
         shutil.copyfile(sys.argv[1]+"_"+str(num)+".jpg", sys.argv[1].split("/")[len(sys.argv[1].split("/"))-1]+"_"+str(num)+".jpg")
